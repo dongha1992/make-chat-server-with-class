c3d168c3cc2c2eea09ccd0ffd586fc5aa679ffdf
김동하 <kamdunga2002@gmail.com>
2021-06-10 22:23:24 +0900

    feat: add UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chat from './server';
 import styled from 'styled-components';
 
 function App() {
+	const [roomList, setRoomList] = useState([]);
 	const userInformation = {
 		id: 3,
 		name: 'dongha',
@@ -10,17 +11,34 @@ function App() {
 		isBlock: false,
 	};
 
-	const chat = new Chat(userInformation);
-	console.log(chat);
+	const chat = new Chat();
 
 	const createChatRoom = () => {
-		chat.createRoom();
+		chat.createRoom(userInformation);
+		setRoomList(chat.roomList);
+		console.log(chat.roomList);
 	};
 
 	return (
 		<Container>
 			<button onClick={createChatRoom}>채팅방 만들기</button>
-			<div></div>
+			<Content>
+				<div>채팅방 리스트</div>
+				<ChatListContainer>
+					{roomList.map((room) => (
+						<>
+							<div>방장: {room.admin.name}</div>
+							<div>참가 유저: {room.joinedUserList}</div>
+							<div>
+								메시지들:
+								{room.messageList.map((item) => (
+									<div>{item.message}</div>
+								))}
+							</div>
+						</>
+					))}
+				</ChatListContainer>
+			</Content>
 		</Container>
 	);
 }
@@ -30,6 +48,15 @@ export default App;
 const Container = styled.div`
 	display: flex;
 	align-items: center;
+	flex-direction: column;
 	width: 100%;
+	height: 100%;
 `;
-const Content = styled.div``;
+const Content = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding-top: 30px;
+	height: 100%;
+`;
+
+const ChatListContainer = styled.div``;
